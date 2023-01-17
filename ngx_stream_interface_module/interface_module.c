fbd3f142575610cfb60ee26ab7abaefe168e7a12
mi1itray.axe <mi1itray.axe@gmail.com>
2023-01-17 10:12:10 +0800

    change name

--- ngx_stream_interface_module/interface_module.c
+++ ngx_stream_interface_module/interface_module.c
@@ -1,9 +1,12 @@
-/*
- * @Author: 张大炮 mi1itray.axe@gmail.com
- * @Date: 2023-01-07 04:30:01
- * @LastEditors: mi1itray.axe mi1itray.axe@gmail.com
- * @LastEditTime: 2023-01-15 17:51:13
- * @Description: 测试使用的nginx stream module
+/**
+ * @file interface_module.c
+ * @author mi1itray.axe (mi1itray.axe@gmail.com)
+ * @brief nginx stream的模块，用于收集nginx.conf配置信息
+ * @version 0.1
+ * @date 2023-01-16
+ * 
+ * @copyright Copyright (c) 2023 mi1itray.axe
+ * 
  */
 #include <ngx_config.h>
 #include <ngx_core.h>
@@ -28,22 +31,21 @@ typedef struct
 
 typedef struct
 {
-    char *name;
-    ngx_uint_t on_off;
+    int *argc_number;
     int (*module_interface)(tran_s *);
-} ngx_stream_test_on_off;
+} ngx_stream_interface_on_off;
 
 typedef struct
 {
     ngx_array_t *rules;
-} ngx_stream_test_srv_conf_t;
+} ngx_stream_interface_srv_conf_t;
 
 static char *
 ngx_stream_interface_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t
-ngx_stream_test_init(ngx_conf_t *cf);
+ngx_stream_interface_init(ngx_conf_t *cf);
 static void *
-ngx_stream_test_create_srv_conf(ngx_conf_t *cf);
+ngx_stream_interface_create_srv_conf(ngx_conf_t *cf);
 
 static ngx_command_t ngx_stream_interface_commands[] = {
 
@@ -62,12 +64,12 @@ static ngx_command_t ngx_stream_interface_commands[] = {
 
 static ngx_stream_module_t ngx_stream_interface_module_ctx = {
     NULL,                 /* preconfiguration */
-    ngx_stream_test_init, /* postconfiguration */
+    ngx_stream_interface_init, /* postconfiguration */
 
     NULL, /* create main configuration */
     NULL, /* init main configuration */
 
-    ngx_stream_test_create_srv_conf, /* create server configuration */
+    ngx_stream_interface_create_srv_conf, /* create server configuration */
     NULL,                            /* merge server configuration */
 };
 
@@ -75,17 +77,17 @@ ngx_module_t ngx_stream_interface_module = {
     NGX_MODULE_V1,
     &ngx_stream_interface_module_ctx, /* module context */
     ngx_stream_interface_commands,    /* module directives */
-    NGX_STREAM_MODULE,           /* module type */
-    NULL,                        /* init master */
-    NULL,                        /* init module */
-    NULL,                        /* init process */
-    NULL,                        /* init thread */
-    NULL,                        /* exit thread */
-    NULL,                        /* exit process */
-    NULL,                        /* exit master */
+    NGX_STREAM_MODULE,                /* module type */
+    NULL,                             /* init master */
+    NULL,                             /* init module */
+    NULL,                             /* init process */
+    NULL,                             /* init thread */
+    NULL,                             /* exit thread */
+    NULL,                             /* exit process */
+    NULL,                             /* exit master */
     NGX_MODULE_V1_PADDING};
 
-static ngx_int_t ngx_stream_test_init(ngx_conf_t *cf)
+static ngx_int_t ngx_stream_interface_init(ngx_conf_t *cf)
 {
     ngx_stream_handler_pt *h;
     ngx_stream_core_main_conf_t *cmcf;
@@ -97,25 +99,24 @@ static ngx_int_t ngx_stream_test_init(ngx_conf_t *cf)
 
 static char *ngx_stream_interface_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    ngx_stream_test_srv_conf_t *ascf = conf;
-    ngx_uint_t on_off = 0;
+    ngx_stream_interface_srv_conf_t *ascf = conf;
     ngx_str_t *value;
-    ngx_stream_test_on_off *rule;
+    ngx_stream_interface_on_off *rule;
 
     value = cf->args->elts;
-    if (value[2].len == 2 && ngx_strcmp(value[2].data, "on") == 0)
+    if (value[2].len == 3 && ngx_strcmp(value[2].data, "off") == 0)
     {
-        on_off = 1;
+        return NGX_CONF_OK;
     }
     if (ascf->rules == NULL)
     {
-        ascf->rules = ngx_array_create(cf->pool, 2, sizeof(ngx_stream_test_on_off));
+        ascf->rules = ngx_array_create(cf->pool, 2, sizeof(ngx_stream_interface_on_off));
     }
     rule = ngx_array_push(ascf->rules);
     if (rule == NULL)
     {
-        ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0,
-                           "test mod get argu error");
+        ngx_conf_log_error(NGX_LOG_ERR, cf, 0,
+                           "ngx_array_push error");
         return NGX_CONF_ERROR;
     }
     rule->name = ngx_palloc(cf->pool, sizeof(ngx_str_t) * value[1].len);
@@ -135,14 +136,14 @@ static char *ngx_stream_interface_rule(ngx_conf_t *cf, ngx_command_t *cmd, void 
     return NGX_CONF_OK;
 }
 
-static void *ngx_stream_test_create_srv_conf(ngx_conf_t *cf)
+static void *ngx_stream_interface_create_srv_conf(ngx_conf_t *cf)
 {
-    ngx_stream_test_srv_conf_t *conf;
+    ngx_stream_interface_srv_conf_t *conf;
 
-    conf = (ngx_stream_test_srv_conf_t *)ngx_pcalloc(cf->pool, sizeof(ngx_stream_test_srv_conf_t));
+    conf = (ngx_stream_interface_srv_conf_t *)ngx_pcalloc(cf->pool, sizeof(ngx_stream_interface_srv_conf_t));
     if (conf == NULL)
     {
-        ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "ngx_stream_test_create_srv_conf error");
+        ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "ngx_stream_interface_create_srv_conf error");
         return NULL;
     }
 
